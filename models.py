5182a4ac64436686a6cf25577878bc86f1838f42
Aaron (Yinghao) Li <71044569+yl4579@users.noreply.github.com>
2023-11-19 16:33:24 -0500

    compatibility with older transformers version #36

--- models.py
+++ models.py
@@ -699,7 +699,7 @@ def load_checkpoint(model, optimizer, path, load_only_params=True, ignore_module
     for key in model:
         if key in params and key not in ignore_modules:
             print('%s loaded' % key)
-            model[key].load_state_dict(params[key])
+            model[key].load_state_dict(params[key], strict=False)
     _ = [model[key].eval() for key in model]
     
     if not load_only_params:
