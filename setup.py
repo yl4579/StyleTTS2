8c224c26c1347f15bf08f0ed0008356a9ba557c5
Mohannad Ehab Barakat <hannod98@yahoo.com>
2024-03-01 21:55:47 +0100

    test

--- setup.py
+++ setup.py
@@ -14,7 +14,10 @@ setup(
    long_description=open('README.md').read(),
    install_requires=required,
    url="https://github.com/yl4579/StyleTTS2.git",
-   include_package_data=True,
+#    include_package_data=True,
+    package_data={
+        'styletts2': ['**/*.txt', '**/*.t7', '**/*.pth', '**/*.json', '**/*.yaml', '**/*.yml']
+    }
 
    
 )
